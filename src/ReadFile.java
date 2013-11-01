ff19dde93292b8a7a6a9296a153f29703bff53f3
Alex Wolff <A.Leon.Wolff@gmail.com>
2013-11-01 14:13:42 -0400

    complete mostly

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -3,7 +3,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 
 public class ReadFile {
@@ -34,20 +33,19 @@ public class ReadFile {
 			//an array for the times of the roll pitch and yaw samples
 			ArrayList<Double> RPYTime = new ArrayList<Double>();
 			//accelerometer sampling rate
-			Double accelSR;
+			Double accelSR = new Double(32);
 			//gyroscope sampling rate
-			Double gyroSR;
+			Double gyroSR = new Double(32);
 			//magnetometer sampling rate
-			Double magnetSR;
+			Double magnetSR = new Double(32);
 			//roll pitch and yaw sampling rate
-			Double RPYSR;
-			double x = 0,y = 0,z = 0,time = 0,lasttime = 0;
+			Double RPYSR = new Double(32);
 			File file = new File(FileName);
 			FileReader fr = new FileReader(file.getAbsoluteFile());
 			BufferedReader br = new BufferedReader(fr);	
-			int sec = 0;
 			//reads first line of file
 			String line = br.readLine();
+			Double startTime = new Double(0);
 			//read in the lines of the file			
 			while(line!=null){
 				//split string into each element
@@ -55,7 +53,7 @@ public class ReadFile {
 				if(lineElems[1].equals("Event")){
 					if(lineElems[2].equals("Test Begin")){
 						//set start time
-						Double startTime = getTime(lineElems[0]);
+						startTime = getTime(lineElems[0]);
 					}
 					else if(lineElems[2].equals("Test End")){
 						break;
@@ -85,62 +83,33 @@ public class ReadFile {
 					roll.add(Double.valueOf(lineElems[17]));
 					pitch.add(Double.valueOf(lineElems[18]));
 					yaw.add(Double.valueOf(lineElems[19]));
-					RPYTime.add(getTime(lineElems[0]));
+					RPYTime.add(getTime(lineElems[0]) - startTime);
+					gyroX.add(Double.valueOf(lineElems[20]));
+					gyroY.add(Double.valueOf(lineElems[21]));
+					gyroZ.add(Double.valueOf(lineElems[22]));
+					gyroTime.add(getTime(lineElems[0]) - startTime);
 					accelX.add(Double.valueOf(lineElems[26]));
 					accelY.add(Double.valueOf(lineElems[27]));
 					accelZ.add(Double.valueOf(lineElems[28]));
-					accelTime.add(getTime(lineElems[0]));
+					accelTime.add(getTime(lineElems[0]) - startTime);
 					accelX.add(Double.valueOf(lineElems[29]));
 					accelY.add(Double.valueOf(lineElems[30]));
 					accelZ.add(Double.valueOf(lineElems[31]));
-					magnetTime.add(getTime(lineElems[0]));
+					magnetTime.add(getTime(lineElems[0]) - startTime);
 				}
-				
-				
-				//scans line of file
-				Scanner sc = new Scanner(line);
-				if(sc.hasNext())
-					//gets time
-					time = Double.valueOf(sc.next().replace(',',' ').trim());			
-				if(sc.hasNext())
-					//gets x accel value
-					x = Double.valueOf(sc.next().replace(',',' ').trim());
-				if(sc.hasNext())
-					//gets y accel value
-					y = Double.valueOf(sc.next().replace(',',' ').trim());
-				if(sc.hasNext())
-					//gets z accel value
-					z = Double.valueOf(sc.next().replace(',',' ').trim());		
-				if(sc.hasNext())
-					//gets the activity
-					activity = sc.next();
-				//if a second of data has been collected or the activity has changed
-				if(((time-lasttime)>=1000 || !lastactivity.equals(activity)) && lasttime!=0){
-					//create new array for new second
-					data.add(new ArrayList<Double>());
-					//increment counter for what second is being recorded
-					sec++;
-					//record what activity the second is for
-					activities.add(lastactivity);
-					lasttime = time;
-				}
-				//for first time through to initialize lasttime
-				else if(lasttime==0){
-					lasttime = time;
-				}
-				//add mag of data to second array
-				data.get(sec).add(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)));
-				lastactivity = activity;
 				//read next line
 				line = br.readLine();
-				sc.close();
 			}
-			//record final activity
-			activities.add(lastactivity);
 			br.close();
 			//create object containing activity array and data arrays
-			DataandActivity DandA = new DataandActivity(data, activities);
-			return DandA;
+			Data data = new Data(accelX, accelY, accelZ,
+					gyroX, gyroY, gyroZ,
+					magnetX, magnetY, magnetZ,
+					roll, pitch, yaw,
+					accelSR, gyroSR, magnetSR, RPYSR,
+					accelTime, gyroTime, magnetTime, 
+					RPYTime);
+			return data;
 		}
 		//return double value of timestamp in string format
 		private static Double getTime(String time){
